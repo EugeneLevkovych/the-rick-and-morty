8f5238baef0c3adee26105b9d455d4cf61e6e5ed
Eugene <eugenelevkovych@yahoo.com>
2025-06-24 16:31:44 +0300

    add header deck

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,17 @@
 import { NavLink } from 'react-router';
+import logoBlack from '../assets/images/logo-black.png';
 
 export default function Header() {
   return (
-    <div>
-      <NavLink to={'/'}>Characters</NavLink>
-      <NavLink to={'locations'}>Locations</NavLink>
-      <NavLink to={'episodes'}>Episodes</NavLink>
-      <hr />
+    <div className="border-b-0 shadow-md py-[6px]">
+      <div className="container flex justify-between items-center px-4 mx-auto">
+        <img src={logoBlack} alt="logo" />
+        <div className="flex gap-6">
+          <NavLink to={'/'}>Characters</NavLink>
+          <NavLink to={'locations'}>Locations</NavLink>
+          <NavLink to={'episodes'}>Episodes</NavLink>
+        </div>
+      </div>
     </div>
   );
 }
