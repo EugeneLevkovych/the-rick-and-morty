8e200ee869c4c600f612b5d87462fb069356660f
Eugene <eugenelevkovych@yahoo.com>
2025-06-28 11:35:42 +0300

    add handle

--- src/Pages/Characters.jsx
+++ src/Pages/Characters.jsx
@@ -4,22 +4,40 @@ import logoBig from '../assets/images/logo-big.png';
 import CharacterCard from '../components/CharacterCard';
 import LoadMoreBtn from '../components/LoadMoreBtn';
 
+const API_URL = 'https://rickandmortyapi.com/api';
+
 export default function Characters() {
-  const [query, setQuery] = useState(null);
   const [cartoonData, setCartoonData] = useState(null);
+  const [search, setSearch] = useState('');
+  const [species, setSpecies] = useState('');
+  const [gender, setGender] = useState('');
+  const [status, setStatus] = useState('');
+  const [pageNumber, setPageNumber] = useState(1);
 
   useEffect(() => {
     async function getData() {
       const res = await fetch(
-        `https://rickandmortyapi.com/api/character/?page=2&name=rick&status=alive`
+        `${API_URL}/character/?page=${pageNumber}&name=${search}&species=${species}&gender=${gender}&status=${status}`
       );
       const data = await res.json();
       // console.log(data);
       setCartoonData(data);
     }
     getData();
-  }, [query]);
+  }, [search, search, species, gender, status]);
+
   console.log(cartoonData);
+
+  async function handleLoadMore() {
+    const res = await fetch(
+      `${API_URL}/character/?page=${pageNumber}&name=${search}&species=${species}&gender=${gender}&status=${status}`
+    );
+    const data = await res.json();
+    setCartoonData(data);
+    setPageNumber(x => x + 1);
+    console.log(data.results);
+  }
+
   return (
     <div className="container pt-6.5 pb-11 cont-p-m">
       <img className="mx-auto mb-4" src={logoBig} alt="Rick & Morty" />
@@ -28,23 +46,43 @@ export default function Characters() {
           <use href="/sprite.svg#icon-leading"></use>
         </svg>
         <input
+          value={search}
           className="inp-border pl-12"
           type="text"
           placeholder="Filter by name..."
-          onChange={e => setQuery(e.target.value)}
+          onChange={e => setSearch(e.target.value)}
         />
-        <select className="inp-border" name="species" id="species">
+        <select
+          value={species}
+          className="inp-border"
+          name="species"
+          id="species"
+          onChange={e => setSpecies(e.target.value)}
+        >
           <option value="human">Human</option>
           <option value="animal">Animal</option>
           <option value="robot">Robot</option>
+          <option value="alien">Alien</option>
         </select>
-        <select className="inp-border" name="gender" id="gender">
+        <select
+          value={gender}
+          className="inp-border"
+          name="gender"
+          id="gender"
+          onChange={e => setGender(e.target.value)}
+        >
           <option value="female">female</option>
           <option value="male">male</option>
           <option value="genderless">genderless</option>
           <option value="unknown">unknown</option>
         </select>
-        <select className="inp-border" name="status" id="status">
+        <select
+          value={status}
+          className="inp-border"
+          name="status"
+          id="status"
+          onChange={e => setStatus(e.target.value)}
+        >
           <option value="alive">alive</option>
           <option value="dead">dead</option>
           <option value="unknown">unknown</option>
@@ -61,7 +99,7 @@ export default function Characters() {
             />
           ))}
       </ul>
-      <LoadMoreBtn />
+      <LoadMoreBtn onClick={handleLoadMore} />
     </div>
   );
 }
--- src/components/LoadMoreBtn.jsx
+++ src/components/LoadMoreBtn.jsx
@@ -1,7 +1,10 @@
-export default function LoadMoreBtn() {
+export default function LoadMoreBtn({ onClick }) {
   return (
     <>
-      <button className="w-38.5 h-9 block text-blue1 rounded-sm shadow-btn uppercase cursor-pointer mx-auto">
+      <button
+        onClick={onClick}
+        className="w-38.5 h-9 block text-blue1 rounded-sm shadow-btn uppercase cursor-pointer mx-auto"
+      >
         Load more
       </button>
     </>
