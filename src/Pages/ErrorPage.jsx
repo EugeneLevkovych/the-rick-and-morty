c37f15f2712e6a4991998779038a321ce2c6261f
Eugene <eugenelevkovych@yahoo.com>
2025-07-11 17:22:28 +0300

    test 3

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -2,7 +2,7 @@ export default function ErrorPage() {
   return (
     <div className="text-center py-10">
       <h1 className="text-2xl font-bold mb-4">Something went wrong</h1>
-      <p>Sorry, we couldn't load this page. Try again later.</p>
+      <p>Sorry, we couldn't load this page.</p>
     </div>
   );
 }
